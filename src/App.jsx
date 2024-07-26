954ec7182e4b5b126f55a8f81eeb16a2ec2b0d2a
Maxzyto <adaaziabugrimaxwell@gmail.com>
2024-07-26 22:44:47 +0000

    onselect event

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,33 @@
-
-import { CORE_CONCEPTS } from './data';
-import Header from './Header.jsx';
-import CoreConcept from './CoreConcerpt.jsx';
-import TabButton from './tabButton.jsx';
+import { CORE_CONCEPTS } from "./data";
+import Header from "./Header.jsx";
+import CoreConcept from "./CoreConcerpt.jsx";
+import TabButton from "./tabButton.jsx";
 
 function App() {
+  function handleSelect() {
+    console.log("Hello World! --selected");
+  }
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
-        <section id='core-concepts'>
+        <section id="core-concepts">
           <h2>Core Concept</h2>
           <ul>
-            <CoreConcept
-              {...CORE_CONCEPTS[0]}
-            />
-            <CoreConcept
-              {...CORE_CONCEPTS[1]}
-            />
-              <CoreConcept
-              {...CORE_CONCEPTS[2]}
-            />
-              <CoreConcept
-              {...CORE_CONCEPTS[3]}
-            />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
 
-        <section id='examples'>
+        <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <li>
-              <TabButton>Component</TabButton>
-              <TabButton>JSX</TabButton>
-              <TabButton>Props</TabButton>
-              <TabButton>State</TabButton>
-            </li>
+            <TabButton onSelect={handleSelect}>Component</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
         </section>
         <h2>Time to get started!</h2>
